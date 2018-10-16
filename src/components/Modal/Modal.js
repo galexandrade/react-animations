a07ac7711878321cfcb3a20f953c43beea968184
Alex Andrade <g.alex.andrade@gmail.com>
2018-10-16 11:40:53 -0300

    Css Keyframes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
-);
+const modal = (props) => {
+    const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClose'];
+    console.log(cssClasses);
+    return (
+        <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>Dismiss</button>
+        </div>
+    );
+}
 
 export default modal;
